6ffeac929c3eb4211dffdb4cf3f57ffec2d194c4
Victor Awugosi <71373171+GOSIjnr@users.noreply.github.com>
2025-11-21 06:37:47 +0100

    [WIP] Refactor ECS to remove Godot Node dependency
    - Refactored Entity, ComponentBase, and all ECS components to remove inheritance from Godot Node and related Godot-specific attributes.
    - Updated component and entity management logic to use pure C# classes, improved event registration/unregistration in StatsComponent, and clarified gravity and movement APIs.
    - Also added new utility methods and documentation, and removed obsolete scene and import files.

--- common/systems/entities/components/EntitySwapComponent.cs
+++ common/systems/entities/components/EntitySwapComponent.cs
@@ -9,15 +9,14 @@ namespace Core.ECS.Components;
 /// Component that allows an entity to trigger a entity to swap with another entity.
 ///
 /// When attached to an <see cref="Entity"/>, this component listens for swap input
-/// (e.g., via <see cref="InputCommand.SwapCharacter"/>). If swap input is detected,
-/// it publishes a <see cref="PlayerSwapEvent"/> on the entity's <see cref="Events.EventBus"/>.
+/// (e.g., via <see cref="InputCommand.SwapCharacterPressed"/>). If swap input is detected,
+/// it publishes a <see cref="PlayerSwapEvent"/> on the entity's <see cref="Entity.EventBus"/>.
 /// </summary>
-[GlobalClass]
 public partial class EntitySwapComponent : ComponentBase, IInputReceiver
 {
 	public void ReceiveInput(InputCommand command)
 	{
-		if (command.SwapCharacter)
+		if (command.SwapCharacterPressed)
 		{
 			LoggerService.Debug($"<{EntityId}> Swap input detected on entity. Publishing PlayerSwapEvent...");
 			EventBus?.Publish(new PlayerSwapEvent());
