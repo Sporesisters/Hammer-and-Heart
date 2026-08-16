9fe0c8a97b9a68ef9985753d924cbaec8b969e10
BabisSt <mpamphsfe@gmail.com>
2026-08-16 20:51:54 +0200

    Basic Hammer logic/ Changed Layers-Masks

--- common/systems/input/PlayerInputHandler.cs
+++ common/systems/input/PlayerInputHandler.cs
@@ -23,7 +23,7 @@ public partial class PlayerInputHandler : InputHandler
 	public override InputCommand CollectInput()
 	{
 		Vector2 moveDirection = Input.GetVector(MOVE_LEFT, MOVE_RIGHT, MOVE_UP, MOVE_DOWN);
-		bool attackPressed = Input.IsActionPressed(ATTACK);
+		bool attackPressed = Input.IsActionJustPressed(ATTACK);
 		bool swapCharacter = Input.IsActionJustPressed(SWAP_CHARACTER);
 		bool kissPressed = Input.IsActionPressed(KISS);
 
